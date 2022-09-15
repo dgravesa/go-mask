b4c1f8a6ee79c68512886b12bb60cc55835b3b39
Daniel Graves <dgravesa@gmail.com>
2022-09-14 20:09:28 -0400

    implementing custom function registration

--- funcregistry.go
+++ funcregistry.go
@@ -8,6 +8,16 @@ import (
 // FuncBuilder is used to create a mask.Func given arguments specified in a struct tag.
 type FuncBuilder func(args ...string) (Func, error)
 
+// RegisterCustomMasker registers a new custom masking function which can be used via struct tags.
+func RegisterCustomMasker(funcName string, builder FuncBuilder) error {
+	_, found := funcRegistry[funcName]
+	if found {
+		return fmt.Errorf("mask func name already taken: \"%s\"", funcName)
+	}
+	funcRegistry[funcName] = builder
+	return nil
+}
+
 var funcRegistry = map[string]FuncBuilder{
 	"simple": buildSimpleMaskFunc,
 }
